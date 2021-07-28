41808f3c0072ee6f25eb9a8b57ca5c1ddd8fa5f1
nicola <nicolaccarroll@gmail.com>
2021-07-28 15:56:29 +0100

    ex 10 feature test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,9 @@ require 'docking_station'
 describe DockingStation do
   subject(:docking_station) { described_class.new }
   it { expect(docking_station).to respond_to(:release_bike) }
+
+  it 'when a bike is released it works' do
+    bike = docking_station.release_bike
+    expect(bike).to be_working
+  end
 end
